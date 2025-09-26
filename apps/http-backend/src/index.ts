73a9e963b50bf7ab17285455c831cfda1217ff0f
Aneeshie <aneeshdas556@gmail.com>
2025-09-26 11:41:54 +0530

    feat: db setup

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 
 import {
   CreateRoom,
@@ -8,38 +9,122 @@ import {
   CreateSignUpSchema,
 } from "@repo/common/types";
 
+import { prismaClient } from "@repo/db/client";
+
 dotenv.config({ path: require("path").resolve(__dirname, "../../../.env") });
+
+if (!process.env.JWT_SECRET) {
+  throw new Error("JWT_SECRET environment variable is required");
+}
 
 const app = express();
 
 app.use(express.json());
 
-app.post("/signin", (req, res) => {
-  //db call
+app.post("/signin", async (req, res) => {
   const parsedData = CreateSignInSchema.safeParse(req.body);
   if (!parsedData.success) {
-    return res.json({
+    return res.status(400).json({
+      sucess: false,
       message: "Incorrect inputs",
+      errors: parsedData.error,
+    });
+  }
+
+  try {
+    const user = await prismaClient.user.findUnique({
+      where: {
+        email: parsedData.data.email,
+      },
+    });
+
+    if (!user) {
+      return res.status(404).json({
+        success: false,
+        message: "the user does not exist",
+      });
+    }
+
+    const isAuth = bcrypt.compare(parsedData.data.password, user.password);
+
+    if (!isAuth) {
+      return res.status(401).json({
+        success: false,
+        message: "unauthorized",
+      });
+    }
+
+    const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET!, {
+      expiresIn: "7d",
+    });
+
+    res.status(200).json({
+      success: true,
+      message: "Successfully signed in",
+      name: user.name,
+      token,
+    });
+  } catch (e) {
+    console.log(e);
+    return res.status(500).json({
+      message: "internal server error",
     });
   }
 });
 
-app.post("/signup", (req, res) => {
+app.post("/signup", async (req, res) => {
   //db call
   const parsedData = CreateSignUpSchema.safeParse(req.body);
   if (!parsedData.success) {
-    return res.json({
+    return res.status(400).json({
+      success: false,
       message: "incorrect inputs",
+      errors: parsedData.error,
     });
   }
 
-  const userId = 1;
-  const token = jwt.sign(
-    {
-      userId,
-    },
-    process.env.JWT_SECRET!
-  );
+  try {
+    const existingUser = await prismaClient.user.findUnique({
+      where: {
+        email: parsedData.data.email,
+      },
+    });
+
+    if (existingUser) {
+      return res
+        .json({
+          success: false,
+          message: "the user already exists",
+        })
+        .status(409);
+    }
+    const hashedPassword = await bcrypt.hash(parsedData.data.password, 10);
+
+    const user = await prismaClient.user.create({
+      data: {
+        name: parsedData.data.name,
+        email: parsedData.data.email,
+        password: hashedPassword,
+      },
+    });
+
+    const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET!, {
+      expiresIn: "7d",
+    });
+
+    return res.status(201).json({
+      success: true,
+      message: "user created successfully",
+      data: {
+        name: user.name,
+        token,
+      },
+    });
+  } catch (e) {
+    return res.status(401).json({
+      message: "could not create user",
+    });
+  }
 });
 
 app.post("/room", (req, res) => {
