0bc47d6e1f2d3940d8ef70b7126acd2e27144273
Aneeshie <aneeshdas556@gmail.com>
2025-10-03 11:34:33 +0530

    feat: db and http layer

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,6 +1,9 @@
 import express from "express";
 import { middleware } from "./middleware";
 import jwt from "jsonwebtoken";
+import { prismaClient } from "@repo/db/db";
+import bcrypt from "bcrypt";
+
 import {
   SignInSchema,
   SignUpSchema,
@@ -20,22 +23,41 @@ app.post("/signup", async (req, res) => {
     return res.status(400).json({ error: parseResult.error.message });
   }
 
-  const token = jwt.sign(
-    { userId: req.userId, email: parseResult.data.email },
-    secretKey,
-    { expiresIn: "7d" },
-  );
+  const hashedPassword = await bcrypt.hash(parseResult.data.password, 10);
 
-  res.status(201).json({
-    success: true,
-    data: {
-      userId: req.userId,
-      email: parseResult.data.email,
-      token,
-    },
-  });
+  try {
+    const user = await prismaClient.user.create({
+      data: {
+        email: parseResult.data.email,
+        password: hashedPassword,
+        name: parseResult.data.username,
+      },
+    });
 
-  //db call
+    if (!user) {
+      return res.status(500).json({ error: "Could not create user" });
+    }
+
+    req.userId = user.id;
+
+    const token = jwt.sign(
+      { userId: req.userId, email: parseResult.data.email },
+      secretKey,
+      { expiresIn: "7d" },
+    );
+
+    return res.status(201).json({
+      success: true,
+      data: {
+        userId: req.userId,
+        email: parseResult.data.email,
+        token,
+      },
+    });
+  } catch (e) {
+    console.log(e);
+    return res.status(500).json({ error: "Could not create user" });
+  }
 });
 
 app.post("/signin", async (req, res) => {
@@ -44,30 +66,79 @@ app.post("/signin", async (req, res) => {
   if (!parseResult.success) {
     return res.status(400).json({ error: parseResult.error.message });
   }
-  const token = jwt.sign({ userId: req.userId }, secretKey, {
-    expiresIn: "7d",
-  });
 
-  if (!token) {
-    return res.status(500).json({ error: "Could not create token" });
+  try {
+    const user = await prismaClient.user.findUnique({
+      where: {
+        email: parseResult.data.email,
+      },
+    });
+
+    if (!user) {
+      return res.status(400).json({ error: "Invalid credentials" });
+    }
+
+    const isPasswordValid = await bcrypt.compare(
+      parseResult.data.password,
+      user.password,
+    );
+
+    if (!isPasswordValid) {
+      return res.status(400).json({ error: "Invalid credentials" });
+    }
+
+    req.userId = user.id;
+
+    const token = jwt.sign({ userId: req.userId }, secretKey, {
+      expiresIn: "7d",
+    });
+
+    if (!token) {
+      return res.status(500).json({ error: "Could not create token" });
+    }
+
+    res.status(201).json({
+      success: true,
+      data: {
+        userId: req.userId,
+        email: parseResult.data.email,
+        token,
+      },
+    });
+  } catch (e) {
+    console.log(e);
+    return res.status(500).json({ error: "Could not sign in user" });
   }
-
-  res.status(201).json({
-    success: true,
-    data: {
-      userId: req.userId,
-      email: parseResult.data.email,
-      token,
-    },
-  });
 });
 
-app.post("/createRoom", middleware, async (req, res) => {
+app.post("/room", middleware, async (req, res) => {
   const parseResult = CreateRoomSchema.safeParse(req.body);
 
   if (!parseResult.success) {
     return res.status(400).json({ error: parseResult.error.message });
   }
-  // db call
-  res.json({ roomId: 456 });
+
+  try {
+    const room = await prismaClient.room.create({
+      data: {
+        name: parseResult.data.name,
+      },
+    });
+
+    await prismaClient.userRoom.create({
+      data: {
+        userId: req.userId!,
+        roomId: room.id,
+      },
+    });
+
+    return res.status(201).json({ success: true, data: room });
+  } catch (e) {
+    console.log(e);
+    return res.status(500).json({ error: "Could not create room" });
+  }
+});
+
+app.listen(3001, () => {
+  console.log("Listening to port 3001");
 });
---
+++ packages/db/prisma/migrations/20251003060122_init_schema/migration.sql
@@ -0,0 +1,61 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "name" TEXT,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Room" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "Room_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "UserRoom" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "userId" TEXT NOT NULL,
+    "roomId" INTEGER NOT NULL,
+
+    CONSTRAINT "UserRoom_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Message" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "content" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "roomId" INTEGER NOT NULL,
+
+    CONSTRAINT "Message_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "UserRoom_userId_roomId_key" ON "UserRoom"("userId", "roomId");
+
+-- AddForeignKey
+ALTER TABLE "UserRoom" ADD CONSTRAINT "UserRoom_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "UserRoom" ADD CONSTRAINT "UserRoom_roomId_fkey" FOREIGN KEY ("roomId") REFERENCES "Room"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Message" ADD CONSTRAINT "Message_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Message" ADD CONSTRAINT "Message_roomId_fkey" FOREIGN KEY ("roomId") REFERENCES "Room"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
